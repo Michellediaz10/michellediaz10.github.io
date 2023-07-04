ef91a7969f6d614aceb0fa36f05d13d15b854d79
Michelle <ddaihana10@gmail.com>
2023-07-04 10:26:22 -0500

    mostrar enlaces

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.onload = function () {
     "0IlUVfWThdo",
     "imagen2.png", //Sesion 2- Calendarios escolares
     "imagen3.png", //sesion 3- acerca de nosotros
-    "imagen4.png",//sesion 3
+    "imagen4.png", //sesion 3
     "r0HVCFzrJnU",
     "imagen5.png", //sesion 6 - nuestra propuesta pedagogica
     "imagen6.png",
@@ -30,7 +30,6 @@ window.onload = function () {
     "imagen19.png",
     "imagen20.png",
     "imagen21.png",
-
   ];
   var posicionActual = 0;
   let $botonRetroceder = document.querySelector("#retroceder");
@@ -50,10 +49,10 @@ window.onload = function () {
       posicionActual++;
     }
 
-    if (IMAGENES[posicionActual].match(/\.png/i)){ 
+    if (IMAGENES[posicionActual].match(/\.png/i)) {
       renderizarImagen();
-      } else { 
-        renderizarVideo();
+    } else {
+      renderizarVideo();
     }
   }
 
@@ -66,11 +65,11 @@ window.onload = function () {
     } else {
       posicionActual--;
     }
-    if (IMAGENES[posicionActual].match(/\.png/i)){ 
-    renderizarImagen();
-    } else { 
+    if (IMAGENES[posicionActual].match(/\.png/i)) {
+      renderizarImagen();
+    } else {
       renderizarVideo();
-   }
+    }
   }
 
   /**
@@ -81,38 +80,38 @@ window.onload = function () {
     $temp_img = document.createElement("img");
     $temp_img.style.width = "100%";
     $temp_img.src = IMAGENES[posicionActual];
-    
-    // 1.identificar la imagen donde hay que aplicar texto
-    // 2.insertar html desde js (generar una etiqueta p y una etiqueta a que me rediriga a los enlaces)
-    // p adapatar a dispositivos moviles
-    // $temp_img = document.createElement("iframe");
-    // $temp_img.style.width = "100%";
-    // $temp_img.src = IMAGENES[posicionActual];
 
-    $imagen.innerHTML = '';
-    $imagen.appendChild($temp_img)
-     // Obtener los elementos de los botones
-    
-}
+    $imagen.innerHTML = "";
+    $imagen.appendChild($temp_img);
+    if(posicionActual == 18){
+      let temp_html = "<div class='position-relative'><p class='position-absolute end-0'>Conoce los requisitos para obtener el High School Diploma haciendo clic <a href='https://8656972.fs1.hubspotusercontent-na1.net/hubfs/8656972/Comunicado%20pasos%20para%20ver%20registro%20dpto%20florida/GENUINE%20REQUISITOS%20HIGH%20SCHOOL%20TF.pdf' class='oval-button px-3' target='_blank'>Aqui<a></p></div>";
+      $imagen.innerHTML+=temp_html;
+    }
+    if(posicionActual == 26){
+      let temp_html = "<div class='position-relative'><p class='position-absolute end-0'>Conoce los costos para otros países haciendo clic <a href='https://drive.google.com/file/d/1gej_OTZtckdYxYL9_ty-iVx0_39qDFv9/view' class='oval-button px-3' target='_blank'>Aqui<a></p></div><br>";
+      $imagen.innerHTML+=temp_html;
+      let temp_html2 = "<div class='position-relative'><p class='position-absolute end-0'>Comienza ahora tu proceso de admisión en nuestro colegio haciendo clic <a href='https://almahub.co/admisiones' class='oval-button px-3' target='_blank'>Aqui<a></p></div>";
+      $imagen.innerHTML+=temp_html2;
+    }
+  }
 
- 
   function renderizarVideo(url) {
-   var link = "https://www.youtube-nocookie.com/embed/"+IMAGENES[posicionActual];
-   var iframe = document.createElement('iframe');
-   //iframe.class = "video"
-   iframe.frameBorder=0;
-   iframe.width="75%";
-   iframe.height="450px";
-   iframe.style="position: absolute; left: 13%; margin-top: 33px;";
-   iframe.id="randomid";
-   iframe.allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-   iframe.allowFullscreen;
-   iframe.setAttribute("src", link);
-   iframe.setAttribute("class", "video");
-   $imagen.innerHTML = '';
-   $imagen.appendChild(iframe);
-   
-
+    var link =
+      "https://www.youtube-nocookie.com/embed/" + IMAGENES[posicionActual];
+    var iframe = document.createElement("iframe");
+    //iframe.class = "video"
+    iframe.frameBorder = 0;
+    iframe.width = "75%";
+    iframe.height = "450px";
+    iframe.style = "position: absolute; left: 13%; margin-top: 33px;";
+    iframe.id = "randomid";
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowFullscreen;
+    iframe.setAttribute("src", link);
+    iframe.setAttribute("class", "video");
+    $imagen.innerHTML = "";
+    $imagen.appendChild(iframe);
   }
   /**
    * Activa el autoplay de la imagen
@@ -123,52 +122,27 @@ window.onload = function () {
   $botonRetroceder.addEventListener("click", retrocederFoto);
   // Iniciar
   renderizarImagen();
-  document.getElementById("inicio").addEventListener("click",()=>{
+
+  let nav_links = document.getElementsByClassName("nav-link");
+
+  for (let i = 0; i < nav_links.length; i++) {
     
-      posicionActual = 0;
-      if (IMAGENES[posicionActual].match(/\.png/i)){ 
+    nav_links[i].addEventListener("click", function (e) {
+      quitarElementoActivo()
+      posicionActual = nav_links[i].getAttribute('data-posicion');
+      if (IMAGENES[posicionActual].match(/\.png/i)) {
         renderizarImagen();
-        } else { 
-          renderizarVideo();
-      }
-     
-  });
-
-  document.getElementById("calendario").addEventListener("click",()=>{
-    
-    posicionActual = 2;
-    if (IMAGENES[posicionActual].match(/\.png/i)){ 
-      renderizarImagen();
-      } else { 
+      } else {
         renderizarVideo();
-    }
-   
-});
-
- document.getElementById("nosotros").addEventListener("click",()=>{
-    
-      posicionActual = 3;
-      if (IMAGENES[posicionActual].match(/\.png/i)){ 
-        renderizarImagen();
-        } else { 
-          renderizarVideo();
       }
-     
-  });
+      nav_links[i].classList.add("active")
+    });
+  }
 
-  document.getElementById("propuesta").addEventListener("click",()=>{
-    
-    posicionActual = 6;
-    if (IMAGENES[posicionActual].match(/\.png/i)){ 
-      renderizarImagen();
-      } else { 
-        renderizarVideo();
+  function quitarElementoActivo(){
+    let nav_links = document.getElementsByClassName("nav-link");
+    for (let i = 0; i < nav_links.length; i++) {
+      nav_links[i].classList.remove("active")
     }
-   
-});
-  // document.getElementById("calendario").addEventListener("click",enviarAlaSeccionPorPosicion(IMAGENES,2));
-  // document.getElementById("nosotros").addEventListener("click",enviarAlaSeccionPorPosicion(IMAGENES,3));
-  // document.getElementById("propuesta").addEventListener("click",enviarAlaSeccionPorPosicion(IMAGENES,6));
+  }
 };
-
-
